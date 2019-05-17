3f09a5866565b2b6cc7269557c4085465870241c
Tom <tom@Toms-MacBook-Pro.local>
2019-05-17 16:35:04 -0700

    add footer/ update styles

--- js/custom.js
+++ js/custom.js
@@ -2,7 +2,7 @@ jQuery(document).ready(function($){
     
     let $doc = $(document);
     $doc.on('scroll', function() {
-        return ($doc.scrollTop() > 100) ? $('.menu-bar').addClass('shrink') :  $(".menu-bar").removeClass("shrink");
+        return ($doc.scrollTop() > 100) ? $('.menu-bar, .logo').addClass('shrink') : $('.menu-bar, .logo').removeClass('shrink');
     });
 
 
