255f82ffb330fefb21789745af4ab74a202e5e38
teloe <tomeloe@gmail.com>
2019-07-23 19:49:10 -0700

    style updates/ nav/ images

--- js/custom.js
+++ js/custom.js
@@ -2,7 +2,15 @@ jQuery(document).ready(function ($) {
 
     let $doc = $(document);
     $doc.on('scroll', function () {
-        return ($doc.scrollTop() > 40) ? $('.menu-bar, .logo').addClass('shrink') : $('.menu-bar, .logo').removeClass('shrink');
+        return ($doc.scrollTop() > 0) ? $('.menu-bar, .logo').addClass('shrink') : $('.menu-bar, .logo').removeClass('shrink');
+    });
+
+    // hamburger menu open/ close
+    $('.menu-container').on('click', function(){
+        $('nav').css('transform', 'translateX(0)');
+    });
+    $('.closebtn').on('click', function() {
+        $('nav').css('transform', 'translateX(-580px)');
     });
 
     $('.testimonials-slider').flexslider({
